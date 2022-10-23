dff012f2bab3e46c65ed401b229703e41f0fe4fa
Ronny Korneli <ronnykorneli@googlemail.com>
2022-10-23 07:35:40 +0200

    first commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 export default function Form(){
 
     const [email, setEmail] = useState('')
+    const inputValue = useRef(null)
     
 
     const submitHandler = (e)=>{
@@ -12,10 +13,24 @@ export default function Form(){
         setEmail('')
     }
 
+    const submitHandlerTwo = (e)=>{
+        e.preventDefault()
+        console.log(inputValue.current.value)
+    }
+
     return(
         <div className="form">
+            <h1>Two ways to grab the vaules from Input fields</h1>
+            
+            <h2>Onchange and Value</h2>
             <form onSubmit={ submitHandler }>
                 <input type="text" value={email} onChange = { e => setEmail(e.target.value) } />
+                <button>SUBMIT INPUT</button>
+            </form>
+
+            <h2>useRef Hook</h2>
+            <form onSubmit={submitHandlerTwo} >
+                <input type="text" ref={inputValue}/>
                 <button>SUBMIT INPUT</button>
             </form>
         </div>
