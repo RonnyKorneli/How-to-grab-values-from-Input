0b5d76eaacf97f4d11a8cdc2c5ddb054bf43efc8
Ronny Korneli <ronnykorneli@googlemail.com>
2022-10-24 11:48:13 +0200

    update-with-focus-using-useRef

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 export default function Form(){
 
     const [email, setEmail] = useState('')
     const inputValue = useRef(null)
+    const focusInput = useRef(null)
     
-
+    //  Using the value and onChange attribute
     const submitHandler = (e)=>{
         e.preventDefault()
         console.log(email)
@@ -13,10 +14,21 @@ export default function Form(){
         setEmail('')
     }
 
+    // Using useRef Hook
     const submitHandlerTwo = (e)=>{
         e.preventDefault()
         console.log(inputValue.current.value)
     }
+
+    const submitHandlerThree = (e) => {
+        e.preventDefault()
+        console.log(focusInput.current.value)
+    }
+
+    // Making input field focus on page load
+    useEffect(() => {
+        focusInput.current.focus()
+    }, [])
 
     return(
         <div className="form">
@@ -31,6 +43,12 @@ export default function Form(){
             <h2>useRef Hook</h2>
             <form onSubmit={submitHandlerTwo} >
                 <input type="text" ref={inputValue}/>
+                <button>SUBMIT INPUT</button>
+            </form>
+
+            <h2>Focus input with useRef Hook</h2>
+            <form onSubmit={submitHandlerThree} >
+                <input type="text" ref={focusInput}/>
                 <button>SUBMIT INPUT</button>
             </form>
         </div>
